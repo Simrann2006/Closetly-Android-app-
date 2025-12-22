b05d13539efe0c65e1c5fd83490feb0d13995ec6
sonu7618 <sonukeshang08@gmail.com>
2025-12-22 13:21:27 +0545

    Modified homescreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation("com.google.accompanist:accompanist-pager:0.36.0")
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.36.0")
 }
--- app/src/main/java/com/example/closetly/HomeScreen.kt
+++ app/src/main/java/com/example/closetly/HomeScreen.kt
@@ -1,20 +1,216 @@
 package com.example.closetly
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.delay
 
 @Composable
 fun HomeScreen() {
+
+    val images = listOf(
+        R.drawable.image1,
+        R.drawable.image2,
+        R.drawable.image3,
+        R.drawable.image4,
+    )
+
+    val pagerState = rememberPagerState()
+
+    LaunchedEffect(pagerState) {
+        while (true) {
+            delay(3000)
+            val nextPage = (pagerState.currentPage + 1) % images.size
+            pagerState.animateScrollToPage(nextPage)
+        }
+    }
+
+    val scrollState = rememberScrollState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Red)
+            .padding(16.dp)
+            .verticalScroll(scrollState),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Home Screen")
+        HorizontalPager(
+            count = images.size,
+            state = pagerState,
+        ) { indexOfImages ->
+
+            Image(
+                painter = painterResource(id = images[indexOfImages]),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth()
+                    .height(300.dp)
+                    .clip(MaterialTheme.shapes.medium),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Spacer(Modifier.height(10.dp))
+
+        HorizontalPagerIndicator(
+            pagerState = pagerState,
+            pageCount = 4,
+            activeColor = Color.White,
+            inactiveColor = MaterialTheme.colors.onSurface.copy(alpha = 0.3f)
+        )
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 12.dp),
+
+            verticalAlignment = Alignment.CenterVertically,
+
+        ) {
+            Image(
+                painter = painterResource(R.drawable.image4),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.LightGray, CircleShape)
+            )
+            Spacer(modifier = Modifier.width(13.dp))
+
+            Text("Emily",
+                style = TextStyle(
+                    fontSize = 23.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Spacer(modifier = Modifier.width(50.dp))
+
+            androidx.compose.material3.Button(
+                onClick = {
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .height(32.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(R.color.purple_200),
+                ),
+                shape = RoundedCornerShape(8.dp),
+            ) {
+                Text(
+                    "Follow", style = TextStyle(
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Icon(
+                painter = painterResource(R.drawable.edit),
+                null,
+                modifier = Modifier.size(50.dp)
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Image(painter = painterResource(R.drawable.lu),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth(),
+
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(
+                    painter = painterResource(R.drawable.img),
+                    null,
+                    modifier = Modifier.size(25.dp)
+                )
+                Text("1143",style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                ))
+                Spacer(modifier = Modifier.width(5.dp))
+
+                Icon(
+                    painter = painterResource(R.drawable.chat),
+                    null,
+                    modifier = Modifier.size(25.dp)
+                )
+                Text("59",style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                ))
+                Spacer(modifier = Modifier.width(5.dp))
+
+                Icon(
+                    painter = painterResource(R.drawable.share),
+                    null,
+                    modifier = Modifier.size(20.dp)
+                )
+                Text("34",style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                ))
+            }
+        Text(
+            "Eco-friendly, wallet-friendly.", style = TextStyle(
+                fontSize = 23.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        )
     }
+}
+
+@Preview
+@Composable
+fun PreviewHome() {
+    HomeScreen()
+    
 }
